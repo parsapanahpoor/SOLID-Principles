f6380ee33ff22e3958e72e6dca8f01b8e58d7103
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-11-22 15:43:48 +0330

    Coupling

---
+++ Coupling/Coupling/Coupling/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Coupling;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+
+#region Tight Coupling - اتصال محکم
+
+public class Yahoo {}
+
+public class SendEmail : Yahoo
+{
+    public void Send(Yahoo gmail) { }
+
+    public List<Yahoo> GetInputs()
+    {
+        return new List<Yahoo>();
+    }
+}
+
+#endregion
+
+
+#region Low Coupling - اتصال سست
+
+public interface ISendMessage
+{
+    void Execute();
+}
+
+public class SendGmail : ISendMessage
+{
+    public void Execute() {}
+}
+
+public class SendYahoo : ISendMessage
+{
+    public void Execute() { }
+}
+
+#endregion
